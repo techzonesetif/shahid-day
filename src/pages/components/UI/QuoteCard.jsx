ffcc0999dc7b1abf8b1af0ce021bd41017521057
mounir habbas <mounirawt@gmail.com>
2025-02-18 20:02:57 +0100

    use memo

--- src/pages/components/UI/QuoteCard.jsx
+++ src/pages/components/UI/QuoteCard.jsx
@@ -1,16 +1,55 @@
-import { faClock, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+// import { faClock, faAngleRight } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import style from '../../home/page.module.css'
+
+// function QuoteCard(props) {
+
+//     const {author, description} = props;
+
+//     return(
+//         <div className={style["quotes-card"]}>
+//             <p className={style["quote-desc"]}>{description}</p>
+//             <h3 className={style["quote-title"]}>{author}</h3>
+//         </div>
+//     )
+// }
+// export default QuoteCard;
+import { memo } from 'react';
+import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import style from '../../home/page.module.css'
+import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
+import style from '../../home/page.module.css';
 
-function QuoteCard(props) {
+const QuoteCard = memo(function QuoteCard({ author, description }) {
+  return (
+    <article 
+      className={style["quotes-card"]}
+      role="article"
+      aria-labelledby={`quote-author-${author}`}
+    >
+      <FontAwesomeIcon 
+        icon={faQuoteLeft} 
+        className={style["quote-icon"]}
+        aria-hidden="true"
+      />
+      <blockquote className={style["quote-desc"]}>
+        {description}
+      </blockquote>
+      <cite 
+        id={`quote-author-${author}`}
+        className={style["quote-title"]}
+      >
+        {author}
+      </cite>
+    </article>
+  );
+});
 
-    const {author, description} = props;
+QuoteCard.propTypes = {
+  author: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+};
 
-    return(
-        <div className={style["quotes-card"]}>
-            <p className={style["quote-desc"]}>{description}</p>
-            <h3 className={style["quote-title"]}>{author}</h3>
-        </div>
-    )
-}
+QuoteCard.displayName = 'QuoteCard';
+
 export default QuoteCard;
